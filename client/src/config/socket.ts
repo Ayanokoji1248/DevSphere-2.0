1159e38497f93f25e680074bf1535a7a38f0a9c1
Krish <krishprajapati1248@gmail.com>
2025-12-03 23:36:00 +0530

    commented out code

--- client/src/config/socket.ts
+++ client/src/config/socket.ts
@@ -1,8 +1,6 @@
 import { io, Socket } from "socket.io-client"
 import { URL } from "../utils"
 
-console.log(URL)
-
 export const socket: Socket = io(URL, {
     autoConnect: false,
     withCredentials: true,
